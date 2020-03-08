aaf30d451fc2b384e81a22796936beaedce06334
barence <44985626+barence@users.noreply.github.com>
2020-03-07 16:00:48 -0800

    Still quite a bit of bugs

---
+++ ogpc game 2020/Assets/Scripts/Enable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enable : MonoBehaviour
+{
+    public bool Gone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+                Gone = !Gone;
+        }
+        if (Gone == (false))
+        {
+            gameObject.GetComponent < BoxCollider2D >().enabled = false;
+          //  gameObject.GetComponent < SpriteRenderer >().sprite. ;
+        }
+        if (Gone == (true))
+        {
+            gameObject.GetComponent < BoxCollider2D >().enabled = true;
+           //gameObject.GetComponent < SpriteRenderer >().sprite. ;
+        }
+    }
+}
